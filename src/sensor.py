ab343c67f7a09d5b06ff9fc70e8092a502311461
20145955 <20145955@tafe.wa.edu.au>
2025-06-09 21:56:35 +0800

    Added sensor base class and subclasses Entry and ExitSensor

--- src/sensor.py
+++ src/sensor.py
@@ -1,12 +1,25 @@
 class CarPark:
     pass
 
-class Sensor(CarPark):
-    def __init__(self, id, is_active, car_park):
+
+class Sensor:
+    """Provides sensors to detect cars"""
+    def __init__(self, id, car_park=None, is_active=False):
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
     def __str__(self):
-        status = "active" if self.is_active else "inactive"
-        return f"Sensor {self.id} ({status})"
+        status = "is active" if self.is_active else "not active"
+        return f"{self.id}: Sensor {status}"
+
+class EntrySensor(Sensor):
+    pass
+
+class ExitSensor(Sensor):
+    pass
+
+s1 = EntrySensor(1, None, True)
+print(s1)
+S2 = ExitSensor(2, None, False)
+print(S2)
