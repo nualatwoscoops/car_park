671a66923d049faa94e505fa543b426d6ff533f1
20145955 <20145955@tafe.wa.edu.au>
2025-06-16 19:34:52 +0800

    Added functionality to make unit tests pass

--- src/car_park.py
+++ src/car_park.py
@@ -11,7 +11,8 @@ class CarPark:
         self.sensors = sensors or []
         self.displays = displays or []
         self.temperature = temperature
-        self.log_file = Path(log_file)
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Welcome to {self.location} car park. {self.capacity} bays are available."
@@ -25,17 +26,19 @@ class CarPark:
         elif isinstance (component, Display):
             self.displays.append(component)
 
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
-        with open(self.log_file, "a") as f:
-            f.write(f"Car added: {plate}\n")
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
-        with open(self.log_file, "a") as f:
-            f.write(f"Car removed: {plate}\n")
+        self._log_car_activity(plate, "exited")
 
     def update_displays(self):
         for display in self.displays:
@@ -50,3 +53,4 @@ class CarPark:
             return 0
         else:
             return calculated_available_bays
+
