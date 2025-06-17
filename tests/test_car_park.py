041739a064e66c3fec5206d0b1b1487861280d08
20145955 <20145955@tafe.wa.edu.au>
2025-06-17 17:36:47 +0800

    Added logging feature to test_car_park

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -31,14 +31,14 @@ class TestCarPark(unittest.TestCase):
     def test_overfill_the_car_park(self):
         for i in range(100):
             self.car_park.add_car(f"FAKE-{i}")
-            self.assertEqual(self.car_park.available_bays, 0)
-            self.car_park.add_car("FAKE-100")
-            # Overfilling the car park should not change the number of available bays
-            self.assertEqual(self.car_park.available_bays, 0)
+        self.assertEqual(self.car_park.available_bays, 0)
+        self.car_park.add_car("FAKE-100")
+        # Overfilling the car park should not change the number of available bays
+        self.assertEqual(self.car_park.available_bays, 0)
 
-            # Removing a car from an overfilled car park should not change the number of available bays
-            self.car_park.remove_car("FAKE-100")
-            self.assertEqual(self.car_park.available_bays, 0)
+        # Removing a car from an overfilled car park should not change the number of available bays
+        self.car_park.remove_car("FAKE-100")
+        self.assertEqual(self.car_park.available_bays, 0)
 
     def test_removing_a_car_that_does_not_exist(self):
         with self.assertRaises(ValueError):
